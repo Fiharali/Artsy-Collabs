c5623bc39df8460c2c354f023f67b366d5d7f769
fihar abdelali <fiharali0@gmail.com>
2024-01-29 16:40:07 +0100

    add migrations

---
+++ app/Http/Controllers/ReseravtionController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reseravtion;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreReseravtionRequest;
+use App\Http\Requests\UpdateReseravtionRequest;
+
+class ReseravtionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReseravtionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reseravtion $reseravtion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Reseravtion $reseravtion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReseravtionRequest $request, Reseravtion $reseravtion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reseravtion $reseravtion)
+    {
+        //
+    }
+}
